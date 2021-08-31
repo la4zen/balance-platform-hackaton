8b49b4edad84dfe63477b457da05b3fdfec2ba5b
Maxim Vasilenko <la4zen@gmail.com>
2021-08-31 16:40:35 +0300

    (add) middleware getting id from jwt

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"github.com/golang-jwt/jwt"
+	"github.com/la4zen/balance-platform-hackaton/pkg/models"
 	"github.com/la4zen/balance-platform-hackaton/pkg/service"
 	"github.com/la4zen/balance-platform-hackaton/pkg/store"
 	"github.com/la4zen/balance-platform-hackaton/pkg/utils"
@@ -17,7 +19,12 @@ func Run() {
 	e.Static("/img", "static")
 	e.POST("/login", service.Login)
 	e.PUT("/register", service.Register)
-	r := e.Group("/api", middleware.JWT(utils.JWT_KEY))
+	r := e.Group("/api", middleware.JWT(utils.JWT_KEY), func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Set("user", &models.User{ID: c.Get("user").(*jwt.Token).Claims.(*models.Claims).ID})
+			return next(c)
+		}
+	})
 	r.GET("/accesible", service.Accesible)
 	e.Logger.Fatal(e.Start(":8080"))
 }
