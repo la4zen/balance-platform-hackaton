97f298f19f8cade7dfdd7c4f366d2992bb2e06c4
Maxim Vasilenko <la4zen@gmail.com>
2021-08-31 17:24:53 +0300

    (edit,fix) struct fields gorm

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -1,11 +1,32 @@
 package models
 
+import "time"
+
 type User struct {
-	ID        uint   `gorm:"primary" json:"id,omitempty"`
+	ID        uint `gorm:"primary" json:"id,omitempty"`
+	IsAdmin   bool
 	Login     string `json:"login,omitempty"`
 	FirstName string `json:"first_name,omitempty"`
 	LastName  string `json:"last_name,omitempty"`
 	Password  string `json:"password,omitempty"`
 	Points    uint   `json:"points,omitempty"`
 	AvatarURI string `json:"avatar_uri,omitempty"`
+}
+
+type Task struct {
+	ID          uint `gorm:"primary"`
+	Name        string
+	Discription string
+}
+
+type TaskProgress struct {
+	ID       uint `gorm:"primary"`
+	User     User `gorm:"embedded;embeddedPrefix:user_"`
+	Task     Task `gorm:"embedded;embeddedPrefix:task_"`
+	Progress uint8
+}
+type Calls struct {
+	ID        uint `gorm:"primary"`
+	TimeStamp time.Time
+	User      User `gorm:"embedded;embeddedPrefix:user_"`
 }
